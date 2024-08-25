18be0255de7cfdfe1a959ebe64f2a5b828e95958
msrissaxox <marissa.bianca.lamothe@gmail.com>
2024-08-25 18:54:26 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import superheroes, { randomSuperhero } from 'superheroes';
+import generateName from 'sillyname';
+
+var sillyName = generateName();
+
+
+console.log(`My name is ${sillyName}`);
+
+
+
+
+console.log(`My superhero name is ${randomSuperhero()}`);
